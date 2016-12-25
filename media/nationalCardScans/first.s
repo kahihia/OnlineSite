a3b21305c4e088b34f5d1173b862c5fcdf996e8c
Sepehr Abdous <sepehrabdous@yahoo.com>
2016-12-25 17:55:57 +0330

    complete merge

---
+++ media/nationalCardScans/first.s
@@ -0,0 +1,39 @@
+	.file	"first.c"
+	.section	.rodata
+.LC0:
+	.string	"%d%d"
+.LC1:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	leaq	-4(%rbp), %rdx
+	leaq	-8(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	__isoc99_scanf
+	movl	-8(%rbp), %edx
+	movl	-4(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
