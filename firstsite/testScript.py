a7157f82c53f6e1162e5fc575a1070bda03d9abe
negargoli <negargoli93@gmail.com>
2017-01-14 10:22:37 +0330

    test

--- firstsite/testScript.py
+++ firstsite/testScript.py
@@ -1,11 +1,15 @@
-from django.contrib.auth.models import User
-from interpay.models import *
-user  = User.objects.get(username="test2@test.com")
-up = UserProfile.objects.get(user=user)
-#ba =BankAccount(name='usdaccount', owner=up, method=BankAccount.DEBIT, cur_code='USD')
-#ba.save()
-ba = BankAccount.objects.get(owner=up,cur_code='USD')
-ba.delete()
-d=Deposit(account=ba,amount=1000.00,banker=up, date=datetime.now, cur_code='USD')
-d.save()
-print ba.balance
+from webbrowser import browser
+from django.test import Client
+from django.test import TestCase
+
+
+class forgetpassword (TestCase):
+  def testsetup(self):
+   response = self.client.get("/login/")
+   print(response.content)
+   self.assertContains(response, 'Forgot password or account disabled')
+
+  def test_wrong_password(self):
+    response = self.client.get
+
+# Create your tests here.
