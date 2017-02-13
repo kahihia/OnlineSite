bc25a23aeddd171e8870bdb9a129268175c8c578
anaeimian <armannaeimian@gmail.com>
2017-02-13 21:17:06 +0330

    rest bug fix

--- RestApi/urls.py
+++ RestApi/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from RestApi import views
 
 urlpatterns = [
-    url(r'^users/', views.snippet_list),
     url(r'^get_token/', views.generate_token),
     url(r'^cash_out_order/', views.cash_out_order),
 ]
