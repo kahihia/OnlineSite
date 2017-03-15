5fb34fccdd1defa90cecac0fb97f7d992f59e2c6
Farzane Hashemi <farzane193@gmail.com>
2017-03-15 12:11:08 +0330

    design first  page for advertising

--- firstsite/urls.py
+++ firstsite/urls.py
@@ -64,6 +64,7 @@ urlpatterns = [
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
 
+
     url(r'^verif/$', views.verify_user, name='verif'),
     url(r'^sms/$', views.send_sms, name='sms'),
     url(r'^retrieve_pass/$', views.retrieve_pass, name='retrieve_pass'),
@@ -76,7 +77,7 @@ urlpatterns = [
     url(r'^top-up/', views.recharge_account, name='recharge_account'),
     url(r'^bank-accounts/', views.bank_accounts, name='bank_accounts'),
     url(r'^callback_handler/([0-9]*)', views.zarinpal_callback_handler, name='callback_handler'),
-
+    url (r'^info/$', views.info),
 ]
 
 urlpatterns += i18n_patterns(
@@ -122,5 +123,7 @@ urlpatterns += i18n_patterns(
     url(r'^bank-accounts/', views.bank_accounts, name='bank_accounts'),
     url(r'^$', views.main_page),
     url(r'^callback_handler/([0-9]*)', views.zarinpal_callback_handler, name='callback_handler'),
+    url(r'^contact.html/$', views.contact, name='contact'),
+    url (r'^info/$', views.info),
 )
 
