e38fcd0a8dc466d6824a31d05fdbe2c526d2222e
negargoli <negargoli93@gmail.com>
2017-04-12 12:40:32 +0430

    test for responsive page

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -199,7 +199,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "interpay/static")
 
 from django.utils.translation import ugettext_lazy as _ut
 LANGUAGES = (
