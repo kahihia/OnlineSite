666f749a5521b1aca6c0c399cb058504fd41c686
Salman Marvasti <technoorreports@gmail.com>
2017-04-17 20:07:02 +0430

    added collectstatic to simulate prod environment. updated django requirement to 1.10.3 updated response web tests

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -213,7 +213,7 @@ LOCALE_PATHS = (
     os.path.join(BASE_DIR, 'locale'),
 )
 LOGIN_URL = '/login/'
-LOGOUT_URL = '/login/'
+LOGOUT_URL = '/logout/'
 # serverset change sepehr to salman
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'interpay/static'),
