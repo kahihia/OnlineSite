6dd61f37b330b3f22a9788cb75a4511d7d670dbd
Salman Marvasti <technoorreports@gmail.com>
2017-04-18 20:32:46 +0430

    delete unecessary conversion folder

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'currencies',
-    'conversion',
+  #  'conversion',
     'interpay',
     'django.contrib.sites',
     'allauth',
